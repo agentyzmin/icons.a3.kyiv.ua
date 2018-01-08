36c9fbb80f4daee98ff8091a0cb4bc0da499597b
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2018-01-09 00:09:10 +0200

    Convert app.js into ES.next

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 // Styles
-require('bootstrap-beta/dist/css/bootstrap.min.css')
-require('ilyabirman-likely/release/likely.css')
-require('./main.css')
+import 'bootstrap-beta/dist/css/bootstrap.min.css'
+import 'ilyabirman-likely/release/likely.css'
+import './main.css'
 
 // Scripts
-var likely = require('ilyabirman-likely');
-require('./main.js')
-require('./gtag.js')
+import likely from 'ilyabirman-likely/release/likely-commonjs'
+import './main.js'
+import './gtag.js'
 
 // Finds all the widgets in the DOM and initializes them
-likely.initiate();
+likely.initiate()
