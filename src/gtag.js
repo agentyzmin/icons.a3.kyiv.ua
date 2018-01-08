8e1dd031bb6f1d1a3a2a4b253f6211ea7e169be8
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2018-01-08 23:55:16 +0200

    Fix gtag script and use ES.next there

--- src/gtag.js
+++ src/gtag.js
@@ -1,21 +1,18 @@
 // Global site tag (gtag.js) - Google Analytics
 (function()
 {
-  var script = document.createElement('script')
-      script.src = 'https://www.googletagmanager.com/gtag/js?id=UA-71269206-7'
-      script.async = true
+  const script = document.createElement('script')
+        script.src = 'https://www.googletagmanager.com/gtag/js?id=UA-71269206-7'
+        script.async = true
 
-  document.head.insertBefore(
-    script,
-    document.querySelector("script")
-  )
+  document.head.appendChild(script)
 })()
 
-window.dataLayer = window.dataLayer || [];
+window.dataLayer = window.dataLayer || []
 
 function gtag() {
-    window.dataLayer.push(arguments);
+    window.dataLayer.push(arguments)
 }
 
-gtag('js', new Date());
-gtag('config', 'UA-71269206-7');
+gtag('js', new Date())
+gtag('config', 'UA-71269206-7')
