abac9cca7592113ac102a106b5057ade3945f153
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2018-01-08 23:49:12 +0200

    Integrate Babel 7

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,18 @@ module.exports = {
           'style-loader',
           'css-loader'
         ]
-      }
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            //presets: ['@babel/preset-env']
+            presets: ['env']
+          }
+        }
+      },
     ]
   },
   plugins: [
